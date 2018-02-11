2d77cf83b0208e6462248d12d68d67973c28f393
gatsunoko <gatsunoko@yahoo.co.jp>
2018-02-11 15:44:15 +0900

    トップ画面のランキング変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,9 @@
 class HomesController < ApplicationController
   def index
     @rankings = Picture.all.order(rating: :desc).limit(5).offset(0)
-    @win_counts = Picture.all.order(win: :desc).limit(5).offset(0)
+    @midiamu_ranking = Picture.where('length = ?', 'ミディアム').order(rating: :desc).limit(5).offset(0)
+    @long_ranking = Picture.where('length = ? OR length = ?', 'ロング', 'セミロング').order(rating: :desc).limit(5).offset(0)
     if user_signed_in?
-      @user_rankings = UserPicture.where(user_id: current_user.id).order(rating: :desc).limit(5).offset(0)
       @user_histories = UserPicture.where(user_id: current_user.id).where('win > ?', 0).order(voting_at: :desc).limit(5).offset(0)
     end
   end
