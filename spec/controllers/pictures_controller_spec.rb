e408f1f7452211057da8a381143291296561f316
gatsunoko <gatsunoko@yahoo.co.jp>
2018-02-23 01:01:32 +0900

    Pucture_showのテスト追加

--- spec/controllers/pictures_controller_spec.rb
+++ spec/controllers/pictures_controller_spec.rb
@@ -17,9 +17,19 @@ RSpec.describe PicturesController, type: :controller do
       end
 
       context 'show' do
-        it 'showを表示できる' do 
-          create(:user, id: 1)
-          @picture = create(:picture)
+        render_views
+        it 'スタイリストが投稿したshowを表示できる' do 
+          @user = create(:user, role: 'stylist', admin: false)
+          create(:stylist, user_id: @user.id)
+          @picture = create(:picture, user_id: @user.id)
+          get 'show', params: { id: @picture.id }
+          expect(assigns(:picture)).to eq @picture #アクションが正常に実行
+          expect(response).to render_template :show #テンプレートが正常に表示
+        end
+
+        it '管理者が投稿したshowを表示できる' do 
+          @user = create(:user, role: 'admin', admin: true)
+          @picture = create(:picture, user_id: @user.id)
           get 'show', params: { id: @picture.id }
           expect(assigns(:picture)).to eq @picture #アクションが正常に実行
           expect(response).to render_template :show #テンプレートが正常に表示
