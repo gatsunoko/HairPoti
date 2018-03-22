5f06448ee8408e2181c1a240730c46ed8ed46e34
gatsunokolab <gatsuonrails@gmail.com>
2018-03-23 07:33:45 +0900

    インスタログイン用のcall_back追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -28,20 +28,23 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
     callback_from :twitter
   end
+
+  def instagram
+    callback_from :instagram
+  end
  
   private
- 
-  def callback_from(provider)
-    provider = provider.to_s
- 
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
- 
-    if @user.persisted?
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+    def callback_from(provider)
+      provider = provider.to_s
+   
+      @user = User.find_for_oauth(request.env['omniauth.auth'])
+
+      if @user.persisted?
+        flash[:notice] = "#{provider}でのログインが完了しました。"
+        sign_in_and_redirect @user, event: :authentication
+      else
+        flash[:notice] = "ログインに失敗しました。"
+        redirect_to new_user_registration_url
+      end
     end
-  end
 end
