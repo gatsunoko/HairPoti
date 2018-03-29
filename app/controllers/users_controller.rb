5227ce3d631bc493b4795bfb9dbe7585e065826b
gatsunoko <gatsunoko@yahoo.co.jp>
2018-03-30 04:12:33 +0900

    パスワード変更機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,34 @@
 class UsersController < ApplicationController
   before_action :is_admin, only: [:index]
-
+  before_action :set_user, only: [:show, :password_edit, :password_update]
   def index
     @users = User.all.page(params[:page]).per(20)
   end
 
   def show
-    @user = User.find params[:id]
     if @user.stylist?
       @pictures = Picture.where(user_id: params[:id]).includes(:picture_details).order(id: :desc).page(params[:page]).per(24)
     end
   end
+
+  def password_edit
+  end
+
+  def password_update
+    if @user.update(user_params)
+      sign_in(@user, bypass: true)
+      redirect_to user_path(@user)
+    else
+      render 'password_edit'
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find params[:id]
+    end
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
                                     confirmations: 'users/confirmations',
                                     sessions: "users/sessions",
                                     omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :users, :only => [:index, :show]
+  resources :users, :only => [:index, :show] do
+    member do
+      get :password_edit
+      patch :password_update
+    end
+  end
   
   resources :pictures do
     collection do
