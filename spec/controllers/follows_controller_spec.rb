4164f36c5d8cc98e80bed40116dc2b4563ff9bfc
gatsunokolab <gatsuonrails@gmail.com>
2018-04-01 17:45:56 +0900

    follow controller テスト追加

--- spec/controllers/follows_controller_spec.rb
+++ spec/controllers/follows_controller_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe FollowsController, type: :controller do
       @user2 = create(:user, role: 2, admin: false)
       login_user @user
     end
+    subject do
+      create_list(:follow, follow_count, user_id: @user.id, stylist_id: @user2.id)
+      create_list(:follow, follower_count, user_id: @user2.id, stylist_id: @user.id)
+    end
 
     context 'follow' do
       it '自分はフォローできない' do
@@ -22,6 +26,32 @@ RSpec.describe FollowsController, type: :controller do
         get 'follow', params: { id: @user2.id }, xhr: true
         expect(response.status).to eq 200
         expect(Follow.all.count.to_i).to_not eq follow_count
+      end
+    end
+
+    context 'followers' do
+      render_views
+      let(:follow_count) {5}
+      let(:follower_count) {20}
+
+      it 'フォロワーが20人、自分がフォローしている人が5人の場合、フォロワー20人が表示される' do
+        subject
+        get 'followers', params: { id: @user.id }
+        expect(assigns(:followers).count).to eq 20
+        expect(response).to render_template 'follows/followers'
+      end
+    end
+
+    context 'follows' do
+      render_views
+      let(:follow_count) {5}
+      let(:follower_count) {20}
+
+      it 'フォロワーが20人、自分がフォローしている人が5人の場合、その5人が表示される' do
+        subject
+        get 'follows', params: { id: @user.id }
+        expect(assigns(:followers).count).to eq 5
+        expect(response).to render_template 'follows/follows'
       end
     end
   end
